453af2b1d509f2c9449ef9ebe955c083c1474b51
klanguyen <nnguyen1@my.wctc.edu>
2020-01-31 14:18:48 -0600

    Adjust files to use ViewModel

--- Modas/Modas/Controllers/HomeController.cs
+++ Modas/Modas/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Modas.Models;
+using Modas.ViewModels;
 
 namespace Modas.Controllers
 {
@@ -17,10 +18,25 @@ namespace Modas.Controllers
             repository = repo;
         }
 
-        public ViewResult Index(int page = 1) => View(
+        /*public ViewResult Index(int page = 1) => View(
             repository.Events.Include(e => e.Location)
                 .OrderBy(e => e.TimeStamp)
                 .Skip((page - 1) * PageSize)
-                .Take(PageSize));
+                .Take(PageSize));*/
+
+        public ViewResult Index(int page = 1) => View(new EventListViewModel
+        {
+            Events = repository.Events
+                                .Include(e => e.Location)
+                                .OrderByDescending(e => e.TimeStamp)
+                                .Skip((page - 1) * PageSize)
+                                .Take(PageSize),
+            PageInfo = new PageInfoViewModel
+            {
+                CurrentPageNumber = page,
+                EventsPerPage = PageSize,
+                TotalEvents = repository.Events.Count()
+            }
+        });
     }
 }
