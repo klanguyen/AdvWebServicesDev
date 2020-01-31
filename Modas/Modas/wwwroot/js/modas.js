72a66f9d942915387649823d12ee7368a587461f
klanguyen <nnguyen1@my.wctc.edu>
2020-01-31 14:57:38 -0600

    Add buttons and css

---
+++ Modas/Modas/wwwroot/js/modas.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    initButtons();
+
+    $('#first, #previous, #next, #last').on('click', function () {
+        location.href = "page" + $(this).data('page');
+    });
+
+    function initButtons() {
+        //disable first and previous buttons on 1st page
+        $('#first, #previous').prop('disabled', $('#start').html() == "1");
+        //disable next and last buttons of the last page
+        $('#next, #last').prop('disabled', $('#end').html() == $('#total').html());
+    }
+})
