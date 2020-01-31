860eb08f9bcf824ccd1d5eafef53a16da3544885
klanguyen <nnguyen1@my.wctc.edu>
2020-01-31 14:02:04 -0600

    Add ViewModels

---
+++ Modas/Modas/ViewModels/EventListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Modas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modas.ViewModels
+{
+    public class EventListViewModel
+    {
+        public IEnumerable<Event> Events { get; set; }
+        public PageInfoViewModel PageInfo { get; set; }
+    }
+}
---
+++ Modas/Modas/ViewModels/PageInfoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modas.ViewModels
+{
+    public class PageInfoViewModel
+    {
+        public int TotalEvents { get; set; }
+        public int EventsPerPage { get; set; }
+        public int CurrentPageNumber { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalEvents / EventsPerPage);
+
+        public int PreviousPage => CurrentPageNumber == 1 ? 1 : CurrentPageNumber - 1;
+        public int NextPage => CurrentPageNumber == TotalPages ? CurrentPageNumber : CurrentPageNumber + 1;
+        public int EventRangeStart => (CurrentPageNumber - 1) * EventsPerPage + 1;
+        public int EventRangeEnd => CurrentPageNumber == TotalPages ? TotalEvents : EventRangeStart + EventsPerPage - 1;
+    }
+}
