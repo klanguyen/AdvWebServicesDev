b63e84c421bb0b89eb4f89e4ba64dc4b64986897
klanguyen <nnguyen1@my.wctc.edu>
2020-01-31 20:28:54 -0600

    API GET - All Events

---
+++ Modas/Modas/Controllers/EventController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Modas.Models;
+
+namespace Modas.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventController : Controller
+    {
+        public IEventRepository repository;
+        public EventController(IEventRepository repo) => repository = repo;
+
+
+        [HttpGet]
+        // returns all events (unsorted)
+        public IEnumerable<Event> Get() => repository.Events
+            .Include(e => e.Location);
+    }
+}
