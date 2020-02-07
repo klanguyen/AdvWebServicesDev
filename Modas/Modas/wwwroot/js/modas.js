3268d33269554dfa95c1654b329ea1ccb0854def
klanguyen <nnguyen1@my.wctc.edu>
2020-02-07 17:56:16 -0600

    Add UserFlag

--- Modas/Modas/wwwroot/js/modas.js
+++ Modas/Modas/wwwroot/js/modas.js
@@ -5,6 +5,16 @@
         location.href = "page" + $(this).data('page');
     });
 
+    $('.flag').on('click', function () {
+        if ($(this).data('checked')) {
+            $(this).data('checked', false);
+            $(this).removeClass('fas').addClass('far');
+        } else {
+            $(this).data('checked', true);
+            $(this).removeClass('far').addClass('fas');
+        }
+    })
+
     function initButtons() {
         //disable first and previous buttons on 1st page
         $('#first, #previous').prop('disabled', $('#start').html() == "1");
