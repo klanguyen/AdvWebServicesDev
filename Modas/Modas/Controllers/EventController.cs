48d80c9f504c2fe073d6f775d31b28df8fc35147
Nguyen Nguyen <nnguyen1@my.wctc.edu>
2020-02-10 18:26:11 -0600

    Api View Model

--- Modas/Modas/Controllers/EventController.cs
+++ Modas/Modas/Controllers/EventController.cs
@@ -24,14 +24,21 @@ namespace Modas.Controllers
 
         [HttpGet("page{page:int}")]
         // returns all event by page
-        public EventListViewModel GetPage(int page = 1) =>
-        new EventListViewModel
+        public ApiListViewModel GetPage(int page = 1) =>
+        new ApiListViewModel
         {
-            Events = repository.Events.Include(e => e.Location)
+            Events = repository.Events
+                    .Select(e => new ApiViewEvent
+                    {
+                        EventId = e.EventId,
+                        Flagged = e.Flagged,
+                        TimeStamp = e.TimeStamp,
+                        LocationName = e.Location.Name
+                    })
                 .OrderByDescending(e => e.TimeStamp)
                 .Skip((page - 1) * PageSize)
                 .Take(PageSize),
-            PageInfo = new PageInfoViewModel
+            PagingInfo = new PageInfoViewModel
             {
                 CurrentPageNumber = page,
                 EventsPerPage = PageSize,
---
+++ Modas/Modas/ViewModels/ApiListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modas.ViewModels
+{
+    public class ApiListViewModel
+    {
+        public IEnumerable<ApiViewEvent> Events { get; set; }
+        public PageInfoViewModel PagingInfo { get; set; }
+    }
+    public class ApiViewEvent
+    {
+        public int EventId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool Flagged { get; set; }
+        public string LocationName { get; set; }
+    }
+}
